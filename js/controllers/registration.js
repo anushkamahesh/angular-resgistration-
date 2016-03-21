f51ea674217f5d1ed8be21321a0298d10cb705e9
anushkamahesh <anushkamahesh@bitbucket.org>
2016-03-21 16:15:21 +0530

    adding controllers

---
+++ js/controllers/registration.js
@@ -0,0 +1,6 @@
+/**
+ * Created by anushkamahesh on 3/21/16.
+ */
+myApp.controller('RegistrationController', ['$scope', function ($scope) {
+    $scope.message = 'welcome to my app';
+}]);
---
+++ js/controllers/success.js
@@ -0,0 +1,6 @@
+/**
+ * Created by anushkamahesh on 3/21/16.
+ */
+myApp.controller('SuccessController', ['$scope', function ($scope) {
+    $scope.message = 'welcome to my app';
+}]);
